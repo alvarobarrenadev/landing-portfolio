4051509bb647ed12993b68b7720509d9d3ecb883
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2025-07-04 17:15:21 +0200

    Add Lenis smooth scrolling library and initialize in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
 import '@sass/main.scss';
-import './validator.js'
+import './validator.js';
+import Lenis from "lenis";
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
